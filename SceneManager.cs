1dc02bda0334f9061eb46ddd37f1dfc4fd420079
ihorekghost <presenter2908@gmail.com>
2024-05-01 09:41:15 +0200

    Added

---
+++ SceneManager.cs
@@ -0,0 +1,31 @@
+using Godot;
+using System;
+
+[GlobalClass]
+public partial class SceneManager : Node
+{
+	[Export]
+	public Node CurrentScene = null;
+
+	public TScene SwitchScene<TScene>(TScene newScene) where TScene : Node
+	{
+		//If scene is already set to newScene - return
+		if (CurrentScene == newScene) return newScene;
+
+		//Remove CurrentScene node if it exists
+		CurrentScene?.QueueFree();
+
+		//Add newScene as a child, if it's not null
+		if(newScene != null) AddChild(newScene);
+
+		//Set CurrentScene to newScene
+		CurrentScene = newScene;
+
+		return newScene;
+	}
+
+	public Node SwitchScene(PackedScene newScene)
+	{
+		return SwitchScene(newScene.Instantiate());
+	}
+}
